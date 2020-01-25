c47ff7bbe075b8865323fcfe760bef2ef1d78bf6
sampathkumar <semsampathkumar@gmail.com>
2020-01-25 16:37:30 +0530

    strings and using single and double qouetes and backtick and concatenation

---
+++ strings/string.js
@@ -0,0 +1,12 @@
+const firstName = "sachin";
+const lastName = "tendulkar";
+const anotherName = `ramesh`;
+
+const hi = "my name' is dhoni";
+//backtick
+const hello = `hi she's very "busy"`;
+
+//concat
+const india = "my name is " + anotherName + ". from chennai";
+
+const haha = `what is your name my name is ${firstName}`;
