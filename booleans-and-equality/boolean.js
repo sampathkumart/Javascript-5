cbf45c4d95e928e8a655319cc285315a8baa7b20
sampathkumar <semsampathkumar@gmail.com>
2020-01-27 17:02:03 +0530

    boolean and equality

---
+++ booleans-and-equality/boolean.js
@@ -0,0 +1,23 @@
+// Boolean
+
+const rohit = 156;
+const virat = 165;
+const final = rohit < virat;
+console.log(final);
+
+// Equality
+
+// trible equal check both value and type
+// double equal check value
+
+const a = 100 === 100;
+console.log(a);
+
+const b = 100 === "100";
+console.log(b);
+
+const c = 56 == 56;
+console.log(c);
+
+const d = 56 == "56";
+console.log(d);
