1359fa9ab7152dfba234347eb5e44159873bab28
sampathkumar <semsampathkumar@gmail.com>
2020-02-04 15:30:41 +0530

    DOM creating html in js

---
+++ Dom-creating-html/creating.js
@@ -0,0 +1,8 @@
+const paragraph = document.createElement("p");
+paragraph.textContent = "india won by 5 runs";
+paragraph.classList.add("cricket");
+console.log(paragraph);
+
+const Div = document.createElement("div");
+Div.classList.add("football");
+console.log(Div);
