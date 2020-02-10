c03367ab0b6c093bcf65ea6ff7cb81d1ec3d4a21
sampathkumar <semsampathkumar@gmail.com>
2020-02-10 12:29:04 +0530

    flow control

---
+++ flow-control/flow-control.js
@@ -0,0 +1,30 @@
+// if statements
+
+if (59 < 15) {
+  console.log("hi");
+}
+if (45 > 8) {
+  console.log("hello");
+}
+if (77 > 22) {
+  console.log("haha");
+}
+
+//equality
+
+const name = "virat";
+const last = "dhoni";
+
+if (name === "sachin" || last == "dhoni") {
+  console.log("nice");
+}
+
+// truthy and falsy values
+
+let points;
+
+if (points) {
+  console.log("he earned many points");
+} else {
+  console.log("no points");
+}
