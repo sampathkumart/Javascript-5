3031c45a9bb3dfe21c121b7bd76a1956e3c205e3
sampathkumar <semsampathkumar@gmail.com>
2020-02-11 09:38:54 +0530

    ternaries

---
+++ coercion-ternaries-and-conditional-abuse/coercion.js
@@ -0,0 +1,11 @@
+// ternaries
+const score = 80;
+const hello = score === 50 ? "won" : "loss";
+console.log(hello);
+
+function india() {
+  console.log("india");
+}
+
+const hi = false;
+hi ? india() : null;
