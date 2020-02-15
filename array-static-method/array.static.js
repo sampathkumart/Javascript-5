0f79ef02723bf093870e88c24d9cb955b841edba
sampathkumar <semsampathkumar@gmail.com>
2020-02-15 10:28:12 +0530

    array static method

---
+++ array-static-method/array.static.js
@@ -0,0 +1,13 @@
+// Array.of()
+Array.of("how old are you", 20);
+
+//Object.entries, Object.keys, Object.values
+const hello = {
+  bat: 15,
+  ball: 10,
+  helmet: 5
+};
+
+console.log(Object.entries(hello));
+console.log(Object.keys(hello));
+console.log(Object.values(hello));
