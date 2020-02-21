2b7616585023ae7987a37ba27f643d935c99adae
sampathkumar <semsampathkumar@gmail.com>
2020-02-21 09:29:30 +0530

    new keyword

---
+++ new-keyword/new.js
@@ -0,0 +1,14 @@
+const date = new Date("jan 21, 2018");
+console.dir(date);
+console.log(date.getFullYear());
+
+const score = new Array(15, 46, 89);
+
+function bioData(name, runs, age) {
+  this.name = name;
+  this.runs = runs;
+  this.age = age;
+}
+
+const info = new bioData("Dhoni", 12000, 38);
+console.log(info);
